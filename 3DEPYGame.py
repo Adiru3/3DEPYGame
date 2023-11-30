820ae60099bda0d593c1db2eacb2c3c95a6356e2
Adiru <54512931+Adiru3@users.noreply.github.com>
2023-11-30 13:01:30 +0200

    Add files via upload

--- 3DEPYGame.py
+++ 3DEPYGame.py
@@ -3,8 +3,10 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+import socket
+import threading
 
-vertices = (
+vertices_cube = (
     (1, -1, -1),
     (1, 1, -1),
     (-1, 1, -1),
@@ -15,7 +17,7 @@ vertices = (
     (-1, 1, 1)
 )
 
-edges = (
+edges_cube = (
     (0, 1),
     (1, 2),
     (2, 3),
@@ -30,12 +32,58 @@ edges = (
     (3, 7)
 )
 
-def draw_cube():
+vertices_character = (
+    (-0.5, -1, -0.5),
+    (0.5, -1, -0.5),
+    (0.5, 1, -0.5),
+    (-0.5, 1, -0.5),
+    (-0.5, -1, 0.5),
+    (0.5, -1, 0.5),
+    (0.5, 1, 0.5),
+    (-0.5, 1, 0.5)
+)
+
+edges_character = (
+    (0, 1),
+    (1, 2),
+    (2, 3),
+    (3, 0),
+    (4, 5),
+    (5, 6),
+    (6, 7),
+    (7, 4),
+    (0, 4),
+    (1, 5),
+    (2, 6),
+    (3, 7)
+)
+
+def draw_cube(vertices, edges):
     glBegin(GL_LINES)
     for edge in edges:
         for vertex in edge:
             glVertex3fv(vertices[vertex])
     glEnd()
+
+def handle_client(client_socket, player_position):
+    while True:
+        data = client_socket.recv(1024).decode('utf-8')
+        if not data:
+            break
+        x, y, z = map(float, data.split(','))
+        player_position[:] = [x, y, z]
+
+    client_socket.close()
+
+def multiplayer_server(player_position):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('127.0.0.1', 5555))
+    server_socket.listen()
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        client_handler = threading.Thread(target=handle_client, args=(client_socket, player_position))
+        client_handler.start()
 
 def main():
     pygame.init()
@@ -45,6 +93,11 @@ def main():
     gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
     glTranslatef(0.0, 0.0, -5)
 
+    player_position = [0, 0, 0]
+
+    multiplayer_thread = threading.Thread(target=multiplayer_server, args=(player_position,))
+    multiplayer_thread.start()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -53,7 +106,14 @@ def main():
 
         glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        draw_cube()
+        
+        # Отрисовка куба
+        draw_cube(vertices_cube, edges_cube)
+        
+        # Перемещение и отрисовка персонажа
+        glTranslatef(*player_position)  # Перемещение персонажа
+        draw_cube(vertices_character, edges_character)
+        
         pygame.display.flip()
         pygame.time.wait(10)
 
